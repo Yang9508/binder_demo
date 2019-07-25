9a0fc3fbdaca79f5dec5044d7ccfbf77ce4ea52a
Yang9508 <47219172+Yang9508@users.noreply.github.com>
2019-07-25 16:56:39 -0500

    Add files via upload

---
+++ binder/install.R
@@ -0,0 +1,5 @@
+### install regular packages
+
+install.packages("reticulate") # python support in RMarkdown
+install.packages("ggplot2") # for plotting
+install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
